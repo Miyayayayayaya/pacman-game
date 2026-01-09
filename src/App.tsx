61811dec8a5187e533b2f0e4ff385c54002b55e4
1F10250254 <s1F102502542@iniad.org>
2026-01-10 00:09:45 +0900

    ボード追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
+
+const makeGameBoard=(width:number,height:number):number[][]=>{
+  const twoDimensionalArray:number[][]=Array.from({length:height},()=>Array.from({length:width},()=>0),);
+  return twoDimensionalArray;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [gameBoard, setGameBoard]=useState<number[][]>(makeGameBoard(30,30));
+  const [pos,setPos]=useState({x:1,y:1});
+  useEffect(()=>{
+    const handleKeyDown=(e:KeyboardEvent)=>{
+      console.log(e.key)
+      setPos((prev)=>{
+        switch(e.key){
+          case 'ArrowUp': return {...prev, y:Math.max(1,prev.y-1)};
+          case 'ArrowDown': return {...prev, y:Math.min(prev.y+1,30)};
+          case 'ArrowLeft': return {...prev, x:Math.max(1,prev.x-1)};
+          case 'ArrowRight': return {...prev, x:Math.min(prev.x+1,30)};
+          default: return prev;
+        }
+      });
+    };
+    window.addEventListener('keydown',handleKeyDown);
+    return()=>window.removeEventListener('keydown',handleKeyDown);
+  },[])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={styles.container}>
+      <div className={styles.board}>
+        {gameBoard.map((row,y)=>row.map((col,x)=>(
+          <div className={styles.cell} key={`${x}-${y}`}>
+          </div>
+        )))}
+        <div style={{gridColumn:pos.x,gridRow:pos.y,zIndex:10}}>
+          👤
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
