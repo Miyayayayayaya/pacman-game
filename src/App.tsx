80e481e173fba8aad7990dd70ac98a856fa7c1f4
1F10250254 <s1F102502542@iniad.org>
2026-01-13 18:15:08 +0900

    方向キーの修正

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,46 @@ const makeGameBoard=(width:number,height:number):number[][]=>{
   const twoDimensionalArray:number[][]=Array.from({length:height},()=>Array.from({length:width},()=>0),);
   return twoDimensionalArray;
 }
-
+type Direction='UP'|'DOWN'|'RIGHT'|'LEFT'|'STOP';
 function App() {
   const [gameBoard, setGameBoard]=useState<number[][]>(makeGameBoard(30,30));
-  const [pos,setPos]=useState({x:1,y:1});
+  const [pos,setPos]=useState({x:0,y:0});
+  const [dir,setDir]=useState<Direction>('STOP')
   useEffect(()=>{
     const handleKeyDown=(e:KeyboardEvent)=>{
-      console.log(e.key)
-      setPos((prev)=>{
-        switch(e.key){
-          case 'ArrowUp': return {...prev, y:Math.max(1,prev.y-1)};
-          case 'ArrowDown': return {...prev, y:Math.min(prev.y+1,30)};
-          case 'ArrowLeft': return {...prev, x:Math.max(1,prev.x-1)};
-          case 'ArrowRight': return {...prev, x:Math.min(prev.x+1,30)};
-          default: return prev;
-        }
-      });
+      switch(e.key){
+        case 'ArrowUp': setDir('UP'); break;
+        case 'ArrowDown': setDir('DOWN'); break;
+        case 'ArrowLeft': setDir('LEFT'); break;;
+        case 'ArrowRight': setDir('RIGHT'); break;
+      };
     };
     window.addEventListener('keydown',handleKeyDown);
     return()=>window.removeEventListener('keydown',handleKeyDown);
   },[])
+  useEffect(()=>{
+    if (dir==='STOP') return;
+    const moveInterval=setInterval(()=>{
+      setPos((prev)=>{
+        switch(dir){
+          case 'UP': 
+            if (prev.y > 0) return {...prev,y:prev.y-1};
+            break;
+          case 'DOWN': 
+            if (prev.y < 29) return {...prev,y:prev.y+1};
+            break;
+          case 'LEFT': 
+            if (prev.x > 0) return {...prev,x:prev.x-1};
+            break;
+          case 'RIGHT':
+            if (prev.x < 29) return {...prev,x:prev.x+1};
+            break;
+        }
+        return prev;
+      });
+    },70)
+    return ()=> clearInterval(moveInterval)
+  },[dir])
   return (
     <div className={styles.container}>
       <div className={styles.board}>
@@ -32,7 +52,7 @@ function App() {
           <div className={styles.cell} key={`${x}-${y}`}/>
         )))}
         <div className={styles.character}
-        style={{left:(pos.x-1)*10,top:(pos.y-1)*10,zIndex:10}}>
+        style={{left:(pos.x)*10,top:(pos.y)*10,zIndex:10}}>
           👤
         </div>
       </div>
