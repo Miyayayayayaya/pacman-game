29e68347ae20350e17e31990f545c734a897c130
1F10250254 <s1F102502542@iniad.org>
2026-01-13 19:13:36 +0900

    壁判定の追加、boardの簡易変形

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import { useEffect, useState } from 'react';
 import styles from './App.module.css';
+// import { GameState } from './state/gameState';
 
 const makeGameBoard=(width:number,height:number):number[][]=>{
   const twoDimensionalArray:number[][]=Array.from({length:height},()=>Array.from({length:width},()=>0),);
+  twoDimensionalArray[5][5]=1
   return twoDimensionalArray;
+}
+const setGameSize={
+  x:10,
+  y:10,
 }
 type Direction='UP'|'DOWN'|'RIGHT'|'LEFT'|'STOP';
 function App() {
-  const [gameBoard, setGameBoard]=useState<number[][]>(makeGameBoard(30,30));
+  const [gameBoard, setGameBoard]=useState<number[][]>(makeGameBoard(setGameSize.y,setGameSize.x));
   const [pos,setPos]=useState({x:0,y:0});
   const [dir,setDir]=useState<Direction>('STOP')
+  // const [gameState, setGameState]=useState<GameState>({isGaming:true})
+  // if(gameState.isGaming){
+  //   const makeWall=structuredClone(gameBoard)
+  //   makeWall[5][5]=1;
+  //   setGameBoard(makeWall);
+  // }
   useEffect(()=>{
     const handleKeyDown=(e:KeyboardEvent)=>{
       switch(e.key){
@@ -27,29 +39,41 @@ function App() {
     const moveInterval=setInterval(()=>{
       setPos((prev)=>{
         switch(dir){
-          case 'UP': 
-            if (prev.y > 0) return {...prev,y:prev.y-1};
+          case 'UP':{
+            const nextY=prev.y-1;
+            if (prev.y > 0 && gameBoard[nextY][prev.x]!==1) return {...prev,y:prev.y-1};
             break;
-          case 'DOWN': 
-            if (prev.y < 29) return {...prev,y:prev.y+1};
+          }
+          case 'DOWN':{
+            const nextY=prev.y+1;
+            if (prev.y < setGameSize.y-1 && gameBoard[nextY][prev.x]!==1) return {...prev,y:prev.y+1};
             break;
-          case 'LEFT': 
-            if (prev.x > 0) return {...prev,x:prev.x-1};
+          }
+          case 'LEFT':{
+            const nextX=prev.x-1
+            if (prev.x > 0 && gameBoard[prev.y][nextX]!==1) return {...prev,x:prev.x-1};
             break;
-          case 'RIGHT':
-            if (prev.x < 29) return {...prev,x:prev.x+1};
+          }
+          case 'RIGHT':{
+            const nextX=prev.x+1;
+            if (prev.x < setGameSize.x-1 && gameBoard[prev.y][nextX]!==1) return {...prev,x:prev.x+1};
             break;
+          }
         }
         return prev;
       });
     },70)
     return ()=> clearInterval(moveInterval)
-  },[dir])
+  },[dir,gameBoard])
   return (
     <div className={styles.container}>
-      <div className={styles.board}>
+      <div className={styles.board} style={{
+        width:setGameSize.x*10,
+        height:setGameSize.y*10,
+        gridTemplateColumns: `repeat(${setGameSize.x},10px)`,
+        gridTemplateRows:`repeat(${setGameSize.y},10px)`}}>
         {gameBoard.map((row,y)=>row.map((col,x)=>(
-          <div className={styles.cell} key={`${x}-${y}`}/>
+          <div className={styles.cell} key={`${x}-${y}`} style={{backgroundColor:col===1?'blue':'black'}}/>
         )))}
         <div className={styles.character}
         style={{left:(pos.x)*10,top:(pos.y)*10,zIndex:10}}>
